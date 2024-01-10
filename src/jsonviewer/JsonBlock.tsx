2fa5713486c1e168b38f0b6c9c84a86b18196e5d
bitfexl <54662051+bitfexl@users.noreply.github.com>
2024-01-10 23:17:59 +0100

    added closing for objects and arrays

--- src/jsonviewer/JsonBlock.tsx
+++ src/jsonviewer/JsonBlock.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useContext } from "react";
+import { ReactNode, useContext, useState } from "react";
 import { JsonIndent } from "./JsonIndent";
 import { JsonContext } from "./JsonContext";
 
@@ -9,15 +9,29 @@ export interface JsonBlockProps {
 }
 
 export function JsonBlock({ openingBracket, closingBracket, children }: JsonBlockProps) {
+    const [open, setOpen] = useState(true);
     const jsonCtx = useContext(JsonContext);
 
     return (
         <>
             {openingBracket}
-            <br />
-            {jsonCtx.indentLevel == 0 ? <JsonIndent>{children}</JsonIndent> : children}
-            <br />
-            <JsonIndent addCurrentLevel={jsonCtx.indentLevel > 0 ? -1 : 0}>{closingBracket}</JsonIndent>
+            <span className="inline-flex flex-col align-middle ml-1 mr-1">
+                <button
+                    onClick={() => setOpen(!open)}
+                    className="select-none border-blue-500 border-2 w-4 h-4 rounded-lg font-bold items-top"
+                    style={{ lineHeight: 0 }}
+                >
+                    {open ? "-" : "+"}
+                </button>
+            </span>
+
+            <span style={{ display: open ? "initial" : "none" }}>
+                <br className="select-none" />
+                {jsonCtx.indentLevel == 0 ? <JsonIndent>{children}</JsonIndent> : children}
+                <br />
+            </span>
+
+            {open ? <JsonIndent addCurrentLevel={jsonCtx.indentLevel > 0 ? -1 : 0}>{closingBracket}</JsonIndent> : closingBracket}
         </>
     );
 }
